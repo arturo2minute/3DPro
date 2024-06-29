d60488d9751aba6dc6f6a7cad21bcc1d927544cd
Arturo Diaz <ArturoD@bhsequip.com>
2024-06-28 17:56:55 -0700

    Basic Website

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const formData = new FormData(contactForm);
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const message = formData.get("message");
+
+        // For demonstration purposes, log the form data to the console
+        console.log("Name:", name);
+        console.log("Email:", email);
+        console.log("Message:", message);
+
+        // You can add further processing here, like sending the form data to a server
+        
+
+    });
+});
